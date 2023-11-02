b1942b8a3f6139ee7bc98fcdeeaa50fdc520be22
ngecu <ngecu16@gmail.com>
2023-11-02 20:53:32 +0300

    changed folder structure and done controllers

--- src/controllers/notebookControllers.ts
+++ src/controllers/notebookControllers.ts
@@ -1,69 +1,141 @@
 import { Response, Request } from "express"
-import { addNote, 
-      deleteNote, 
-      getSpecifNote, 
-         getNotes, 
-         updateNote } from "../services/notebookServices";
+import mssql from 'mssql'
+import {  sqlConfig } from "../config/sqlConfig";
+
 
 
 export function TestingRoute(req:Request, res:Response){
       return res.send("Server Running well")
 }
 
-export function getAllNotesController(req: Request, res:Response){
-      let notes = getNotes();
-      res.json(notes)
-}
-
-export function getSpecificNoteController(req:Request, res:Response ){
-      let noteID = parseInt(req.params.noteID);
-     
-      let note = getSpecifNote(noteID);
-
-      res.json(note)
-}
-
-export function deleteNoteController(req:Request, res:Response){
-      let {noteID} = req.params;
-      let parsedID = parseInt(noteID);
-
-      let result = deleteNote(parsedID);
-
-      if(result !== null){
-            res.send(`Note on index: ${result} deleted`);
-      }else{
-            res.send("Note not found")
-      }
-}
-
-export function addNoteController(req: Request, res:Response){
-      let new_note = req.body;
-
-      addNote(new_note);
-      res.json({
-            id: new_note.id,
-            sucess: true
-      })
-}
-
-export async function updateNoteController(req: Request, res: Response) {
+export async function getAllNotes(req:Request, res:Response) {
       try {
-            let { noteID } = req.params;
-      let parsedID = parseInt(noteID);
-      let updatedNote = req.body;
+            const checkNoteQuery = `SELECT * FROM notes`;
     
-
-      await updateNote(updatedNote);
+            mssql.connect(sqlConfig)
+            .then(pool => {
+                return pool.request().query(checkNoteQuery);
+            })
+            .then(async result => {
+                if (result.recordset.length > 0) {
+                    console.log("success", result);
     
-  
-        return res.json({
-          id: parsedID,
-          success: true,
-        });
-      } catch (error) {
-            res.json(error)
-      }
-      
-      
+                    return res.status(200).json(result.recordset)
+    
+                }
+    
+        })
+            
+        } catch (error) {
+            return res.json({
+                error: error
+            })
+        }
     }
     
+    
+    
+    export async function getSpecifNote(req:Request, res:Response) {
+      const note_id = req.params.note_id;
+
+      try {
+          const query = `SELECT * FROM notes WHERE note_id = '${note_id}'`;
+  
+          const pool = await mssql.connect(sqlConfig);
+          const result = await pool.request().query(query);
+  
+          if (result.recordset.length === 0) {
+              return res.status(404).json({ error: 'Note not found' });
+          }
+  
+          const specificNote = result.recordset[0];
+  
+          return res.status(200).json(specificNote);
+      } catch (error) {
+          console.error(error);
+          return res.status(500).json({ error: 'An error occurred while fetching the note profile' });
+      }
+  };
+    
+    
+    
+        export async function addNote(req: Request, res: Response){
+            let { id, title, content } = req.body;
+            let query = `INSERT INTO notes (note_id, title, content) VALUES ('${id}', '${title}', '${content}')`;
+      
+            mssql.connect(sqlConfig).then(pool => {
+                  return pool.request().query(query);
+              }).then(result => {
+                  console.log("success", result);
+            }).catch(err => {
+                  console.log(err);
+      
+                
+                  return res.status(500).json({
+                      error: err.message || 'An error occurred while registering the note.'
+                  });
+              });
+                  
+            }
+            
+          
+    
+    
+              
+            export async function updateNote(req: Request, res: Response) {
+                  try {
+                      let { id, title, content } = req.body;
+                      let query = `
+                          UPDATE notes 
+                          SET title = '${title}', content = '${content}'
+                          WHERE note_id = '${id}'
+                      `;
+              
+                      mssql.connect(sqlConfig)
+                          .then(pool => {
+                              return pool.request().query(query);
+                          })
+                          .then(result => {
+                              console.log("Note updated successfully", result);
+                              res.status(200).json({ message: 'Note updated successfully' });
+                          })
+                          .catch(err => {
+                              console.log(err);
+                              res.status(500).json({ error: err.message || 'An error occurred while updating the note.' });
+                          });
+                  } catch (error) {
+                      console.log(error);
+                      res.status(500).json({ error: 'An error occurred while processing the request.' });
+                  }
+              }
+              
+    
+    
+    
+    
+          
+    
+        
+    
+        export async function deleteNote(req: Request, res: Response){
+            const note_id = req.params.note_id;
+
+            try {
+                const checkNoteQuery = `SELECT * FROM notes WHERE note_id = '${note_id}'`;
+                
+                const pool = await mssql.connect(sqlConfig);
+                const result = await pool.request().query(checkNoteQuery);
+        
+                if (result.recordset.length === 0) {
+                    return res.status(404).json({ error: 'Note not found' });
+                }
+        
+                const deleteQuery = `DELETE FROM notes WHERE note_id = '${note_id}'`;
+                await pool.request().query(deleteQuery);
+        
+                return res.status(200).json({ message: 'Note deleted successfully' });
+            } catch (error) {
+                console.error(error);
+                return res.status(500).json({ error: 'An error occurred while deleting the note' });
+            }
+        }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 // import express from 'express';
 const express = require('express')
-import noterouter from './src/routes/notebookRoutes';
+import noterouter from './routes/notebookRoutes';
 
 
 
